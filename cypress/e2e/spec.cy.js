7ef2425d70345b29b67606020091cb410b27d2b4
QA-Lexx <ilin_001@mail.ru>
2023-08-11 18:45:44 +0300

    create tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,29 @@
 describe('login page', () => {
-  it('logins succesfully', () => {
+
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
+
+  it('logins succesfully', () => {
+    cy.login('test@test.com', 'test')
+    cy.contains('Добро пожаловать test@test.com').should('be.visible')
+    cy.contains('Log out').should('be.visible')
+  })
+
+  it('logins error on empty login', () => {
+    cy.login(null, 'test')
+    cy.get('#mail').then((el) => {
+      expect(el[0].checkValidity()).to.be.false
+      expect(el[0].validationMessage).to.be.eql('Заполните это поле.')
+    })
+  })
+
+  it('logins error on empty password', () => {
+    cy.login('test@test.com', null)
+    cy.get('#pass').then((el) => {
+      expect(el[0].checkValidity()).to.be.false
+      expect(el[0].validationMessage).to.be.eql('Заполните это поле.')
+    })
+  })
+
 })
