cb494a423d22d34f7db9a89d8e6c65b57a173649
QA-Lexx <ilin_001@mail.ru>
2023-08-12 15:40:26 +0300

    add new tests in project

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -26,4 +26,25 @@ describe('login page', () => {
     })
   })
 
+  it('add new book', () => {
+    cy.login('test@test.com', 'test')
+    cy.contains('Add new').click()
+    cy.get('#title').type('Ulysses')
+    cy.get('#description').type('Joyce')
+    cy.contains('Submit').click()
+    cy.contains('Ulysses').should('be.visible')
+  })
+
+  it('addition to favorities', () => {
+    cy.login('test@test.com', 'test')
+    cy.get('[href="book/b6334cd7-1746-40c5-a5ed-e776072efe5d"] > .h-100 > .card-footer > .btn').click()
+    cy.contains('Delete from favorite').should('be.visible')
+  })
+
+  it('book download', () => {
+    cy.login('test@test.com', 'test')
+    cy.get('[href="book/b6334cd7-1746-40c5-a5ed-e776072efe5d"] > .h-100 > .card-body').click()
+    cy.contains('Dowload book').click()
+  })
+
 })
